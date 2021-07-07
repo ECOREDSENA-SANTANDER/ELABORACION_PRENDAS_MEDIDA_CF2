7555213e711f1fc279d56fb408c215c4781d6a69
Edgar Mauricio Cortes <edgarm.cortes@misena.edu.co>
2021-07-07 16:24:22 -0500

    Ajustes

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -31,98 +31,101 @@
     //.justify-content-center
     //.align-items-center
 
-    //- .row.mb-4
-    //-   .col-7.d-flex.align-items-center
-    //-     div
-    //-       p Una tendencia hace referencia a una inclinación o disposición (tangible o intangible) que tiene un grupo, comunidad o sociedad hacia un elemento o grupos de elementos, por lo que una tendencia puede originar gustos por elementos de diferentes áreas, incluso generar coincidencia o adaptación de ideas y hasta corrientes que surgen de una sociedad. 
-    //-       p Como se ha mencionado anteriormente, las tendencias tienden a nacer de múltiples factores que se representan en el sistema moda, funcionando a su vez como respuesta a un fenómeno cargado de simbolismos en el vestuario lo cual produce que se masifique dicha tendencia.  En el sector de la moda las tendencias están unidas a su vez a los estilos de los consumidores, es por ello que es importante entenderlas para comprender el porqué de su aplicabilidad en el sector.
-    //-   .col-5.d-flex.align-items-center.justify-content-center
-    //-     figure
-    //-       img(src='@/assets/curso/tema2/img01.svg', alt='Texto que describa la imagen')
+    //- Desde aca comentario
+    .row.mb-4
+      .col-7.d-flex.align-items-center
+        div
+          p Una tendencia hace referencia a una inclinación o disposición (tangible o intangible) que tiene un grupo, comunidad o sociedad hacia un elemento o grupos de elementos, por lo que una tendencia puede originar gustos por elementos de diferentes áreas, incluso generar coincidencia o adaptación de ideas y hasta corrientes que surgen de una sociedad. 
+          p Como se ha mencionado anteriormente, las tendencias tienden a nacer de múltiples factores que se representan en el sistema moda, funcionando a su vez como respuesta a un fenómeno cargado de simbolismos en el vestuario lo cual produce que se masifique dicha tendencia.  En el sector de la moda las tendencias están unidas a su vez a los estilos de los consumidores, es por ello que es importante entenderlas para comprender el porqué de su aplicabilidad en el sector.
+      .col-5.d-flex.align-items-center.justify-content-center
+        figure
+          img(src='@/assets/curso/tema2/img01.svg', alt='Texto que describa la imagen')
 
-    //- .row.mb-4
-    //-   .col-6.d-flex
-    //-     .cajon.color-primario.fondo-a.p-4.mb-4        
-    //-       Strong ¿De dónde nacen las tendencias de moda?
-    //-       p Hay varios aspectos y sucesos que permiten el nacimiento de tendencias las cuales son tomadas por la moda para su expresión en el vestuario, estas pueden tener su origen en:
-    //-       .row
-    //-         .col-1
-    //-         .col-11
-    //-           ul.lista-ul--color
-    //-             li 
-    //-               i(style="color: #EDE3B7").far.fa-dot-circle
-    //-               p Las macrotendencias.
-    //-             li 
-    //-               i(style="color: #EDE3B7").far.fa-dot-circle
-    //-               p Las pasarelas de los grandes diseñadores del mundo.
-    //-             li 
-    //-               i(style="color: #EDE3B7").far.fa-dot-circle
-    //-               p El análisis y conocimiento del consumidor.
-    //-             li 
-    //-               i(style="color: #EDE3B7").far.fa-dot-circle
-    //-               p Los referentes influyentes en la sociedad, como los grandes personajes que son seguidos por millones de individuos.
+    .row.mb-4
+      .col-6.d-flex
+        .cajon.color-primario.fondo-a.p-4.mb-4        
+          Strong ¿De dónde nacen las tendencias de moda?
+          p Hay varios aspectos y sucesos que permiten el nacimiento de tendencias las cuales son tomadas por la moda para su expresión en el vestuario, estas pueden tener su origen en:
+          .row
+            .col-1
+            .col-11
+              ul.lista-ul--color
+                li 
+                  i(style="color: #EDE3B7").far.fa-dot-circle
+                  p Las macrotendencias.
+                li 
+                  i(style="color: #EDE3B7").far.fa-dot-circle
+                  p Las pasarelas de los grandes diseñadores del mundo.
+                li 
+                  i(style="color: #EDE3B7").far.fa-dot-circle
+                  p El análisis y conocimiento del consumidor.
+                li 
+                  i(style="color: #EDE3B7").far.fa-dot-circle
+                  p Los referentes influyentes en la sociedad, como los grandes personajes que son seguidos por millones de individuos.
 
-    //-   .col-6
-    //-     .cajon.color-acento-contenido.fondo-b.p-4.mb-4        
-    //-       Strong ¿Las tendencias son impuestas?
-    //-       p No, porque estas nacen de un contexto histórico, social, cultural, político, religioso y lingüístico, es decir, es una construcción colectiva de todos los seres humanos porque nacen de la sociedad misma y de su evolución en cuanto a sus demandas con el mundo. Sus principales características son:
-    //-       .row
-    //-         .col-1
-    //-         .col-11
-    //-           ul.lista-ul--color
-    //-             li 
-    //-               i.far.fa-dot-circle
-    //-               strong.me-1 Ritmo:
-    //-               | forma en la que sucede y se alternan una serie de dinámicas, conceptos y sentires que componen la tendencia
-    //-             li 
-    //-               i.far.fa-dot-circle
-    //-               strong.me-1 Información:
-    //-               | toda la variedad de información que pueden complementar el concepto de la tendencia.
-    //-             li 
-    //-               i.far.fa-dot-circle
-    //-               strong.me-1 Diversificación:
-    //-               | diferentes variables en donde se puede manifestar la tendencia, sus conceptos e historias.
-    //-             li 
-    //-               i.far.fa-dot-circle
-    //-               strong.me-1 Tecnología:
-    //-               | medios por los cuales se revela la información y su implementación.
+      .col-6
+        .cajon.color-acento-contenido.fondo-b.p-4.mb-4        
+          Strong ¿Las tendencias son impuestas?
+          p No, porque estas nacen de un contexto histórico, social, cultural, político, religioso y lingüístico, es decir, es una construcción colectiva de todos los seres humanos porque nacen de la sociedad misma y de su evolución en cuanto a sus demandas con el mundo. Sus principales características son:
+          .row
+            .col-1
+            .col-11
+              ul.lista-ul--color
+                li 
+                  i.far.fa-dot-circle
+                  strong.me-1 Ritmo:
+                  | forma en la que sucede y se alternan una serie de dinámicas, conceptos y sentires que componen la tendencia
+                li 
+                  i.far.fa-dot-circle
+                  strong.me-1 Información:
+                  | toda la variedad de información que pueden complementar el concepto de la tendencia.
+                li 
+                  i.far.fa-dot-circle
+                  strong.me-1 Diversificación:
+                  | diferentes variables en donde se puede manifestar la tendencia, sus conceptos e historias.
+                li 
+                  i.far.fa-dot-circle
+                  strong.me-1 Tecnología:
+                  | medios por los cuales se revela la información y su implementación.
               
-    //- p.mb-4.text-center 
-    //-   strong De acuerdo con la información anterior, las tendencias de moda se pueden clasificar en: 
+    p.mb-4.text-center 
+      strong De acuerdo con la información anterior, las tendencias de moda se pueden clasificar en: 
     
-    //- TabsA.color-acento-contenido.mb-4
-    //-   //- .tarjeta debe ir acompañado de una de una de estas clases => 
-    //-   //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
-    //-   //- estas clases tambien tienen un modificador --borde
-    //-   .tarjeta.color-acento-contenido--borde.p-4(titulo="Macrotendencias")
-    //-     .row
-    //-       .col-10.d-flex.align-items-center
-    //-         p Tendencias donde se involucran muchas áreas, sistemas productivos y aspectos emocionales que no solo toca a un país, sino que hace mover al mundo entero; un ejemplo de ello es la sostenibilidad en pro del cuidado del medioambiente, otro puede ser la importancia de la salud y hábitos saludables, generando fenómenos a gran escala como el vegano.
-    //-       .col-2.d-flex.align-items-center.justify-content-center
-    //-         figure
-    //-           img(src='@/assets/curso/tema2/img02.svg', alt='Texto que describa la imagen')
-    //-   .tarjeta.color-acento-contenido--borde.p-4(titulo="Metatendencias")
-    //-     .row
-    //-       .col-10.d-flex.align-items-center
-    //-         p Enfocadas hacia un cambio multidimensional, por oposición a un cambio lineal o secuencial. Son los cambios sustanciales en la estructura socioeconómica (Novik, 2020), hay autores que afirman que las metatendencias son determinantes para la formación de las macrotendencias.
-    //-       .col-2.d-flex.align-items-center.justify-content-center
-    //-         figure
-    //-           img(src='@/assets/curso/tema2/img03.svg', alt='Texto que describa la imagen')
-    //-   .tarjeta.color-acento-contenido--borde.p-4(titulo="Microtendencias")
-    //-     .row
-    //-       .col-10.d-flex.align-items-center
-    //-         p Son originadas por las macrotendencias, pero son pasajeras, cambian cada temporada, revolucionan el mercado, abren un abanico de posibilidades y promueven el consumo masivo.
-    //-       .col-2.d-flex.align-items-center.justify-content-center
-    //-         figure
-    //-           img(src='@/assets/curso/tema2/img04.svg', alt='Texto que describa la imagen')
-    //-   .tarjeta.color-acento-contenido--borde.p-4(titulo="Difusión de tendencias")
-    //-     .row
-    //-       .col-10.d-flex.align-items-center
-    //-         p La moda posibilita el hecho de que se difundan las tendencias que generan el mismo fenómeno “adaptación por las masas al uso de algo”. Si bien la moda se basa en ellas, hay que tener presente que para muchos la moda es distinción y en muchas ocasiones las personas se vuelven imitadoras de sus referentes, de ahí que sean influyentes en la creación de tendencias de moda.
-    //-       .col-2.d-flex.align-items-center.justify-content-center
-    //-         figure
-    //-           img(src='@/assets/curso/tema2/img05.svg', alt='Texto que describa la imagen')
+    TabsA.color-acento-contenido.mb-4
+      //- .tarjeta debe ir acompañado de una de una de estas clases => 
+      //- .color-primario, .color-secundario, .color-acento-contenido, .color-acento-botones
+      //- estas clases tambien tienen un modificador --borde
+      .tarjeta.color-acento-contenido--borde.p-4(titulo="Macrotendencias")
+        .row
+          .col-10.d-flex.align-items-center
+            p Tendencias donde se involucran muchas áreas, sistemas productivos y aspectos emocionales que no solo toca a un país, sino que hace mover al mundo entero; un ejemplo de ello es la sostenibilidad en pro del cuidado del medioambiente, otro puede ser la importancia de la salud y hábitos saludables, generando fenómenos a gran escala como el vegano.
+          .col-2.d-flex.align-items-center.justify-content-center
+            figure
+              img(src='@/assets/curso/tema2/img02.svg', alt='Texto que describa la imagen')
+      .tarjeta.color-acento-contenido--borde.p-4(titulo="Metatendencias")
+        .row
+          .col-10.d-flex.align-items-center
+            p Enfocadas hacia un cambio multidimensional, por oposición a un cambio lineal o secuencial. Son los cambios sustanciales en la estructura socioeconómica (Novik, 2020), hay autores que afirman que las metatendencias son determinantes para la formación de las macrotendencias.
+          .col-2.d-flex.align-items-center.justify-content-center
+            figure
+              img(src='@/assets/curso/tema2/img03.svg', alt='Texto que describa la imagen')
+      .tarjeta.color-acento-contenido--borde.p-4(titulo="Microtendencias")
+        .row
+          .col-10.d-flex.align-items-center
+            p Son originadas por las macrotendencias, pero son pasajeras, cambian cada temporada, revolucionan el mercado, abren un abanico de posibilidades y promueven el consumo masivo.
+          .col-2.d-flex.align-items-center.justify-content-center
+            figure
+              img(src='@/assets/curso/tema2/img04.svg', alt='Texto que describa la imagen')
+      .tarjeta.color-acento-contenido--borde.p-4(titulo="Difusión de tendencias")
+        .row
+          .col-10.d-flex.align-items-center
+            p La moda posibilita el hecho de que se difundan las tendencias que generan el mismo fenómeno “adaptación por las masas al uso de algo”. Si bien la moda se basa en ellas, hay que tener presente que para muchos la moda es distinción y en muchas ocasiones las personas se vuelven imitadoras de sus referentes, de ahí que sean influyentes en la creación de tendencias de moda.
+          .col-2.d-flex.align-items-center.justify-content-center
+            figure
+              img(src='@/assets/curso/tema2/img05.svg', alt='Texto que describa la imagen')
     
+
+    //- Hasta aca comentario
     p.mb-3.text-center
       strong Así, para la comprensión de la difusión de tendencias es necesario comprender las siguientes teorías:
 
